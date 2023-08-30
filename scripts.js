b33cf45979826271af98d2a0f17bc813b946c360
Kardo Heydari <120682793+imkardo@users.noreply.github.com>
2023-08-30 22:32:31 +0330

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
-'use strict'
+'use strict';
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = '🎉 Correct Number !'
+// document.querySelector('.number').textContent = 10;
+// document.querySelector('.score').textContent = 13;
+
+document.querySelector('.check').addEventListener('click', function () {
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
+    if (!guess) {
+        document.querySelector('.message').textContent = '⛔ No Number!'
+    }
+})
