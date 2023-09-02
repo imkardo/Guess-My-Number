c1fec2d8869c3a0624cca83c59f77d96da2566fd
Kardo Heydari <120682793+imkardo@users.noreply.github.com>
2023-09-02 12:18:50 +0330

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,8 +3,11 @@
 // document.querySelector('.message').textContent = '🎉 Correct Number !'
 // document.querySelector('.number').textContent = 10;
 // document.querySelector('.score').textContent = 13;
-let score = 20;
+
+
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
 
@@ -18,7 +21,10 @@ document.querySelector('.check').addEventListener('click', function () {
         document.querySelector('body').style.backgroundColor = '#60b347';
         document.querySelector('.number').style.width = '30rem';
         document.querySelector('.number').textContent = secretNumber;
-
+        if (score > highscore) {
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore;
+        }
         // When Guess Is Too High
     } else if (guess > secretNumber) {
         if (score > 1) {
